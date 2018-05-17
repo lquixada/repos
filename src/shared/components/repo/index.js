6c996378b2d01b288cddee4a318e0586cf77c124
Leonardo Quixadá <lquixada@gmail.com>
2018-05-17 11:22:43 -0300

    moved repo selection to Repo Container.

--- src/shared/components/repo/index.js
+++ src/shared/components/repo/index.js
@@ -3,29 +3,15 @@ import styled from 'styled-components';
 
 import {Title} from './title';
 
-export class Repo extends React.Component {
-  getRepo() {
-    const {repos, match} = this.props;
-    return repos.find((repo) => repo.name === match.params.repo);
-  }
-
-  render() {
-    const repo = this.getRepo();
-
-    if (!repo) {
-      return null;
-    }
-
-    return (
-      <Wrapper>
-        <Title>{repo.name}</Title>
-        <pre>
-          {JSON.stringify(repo, null, '\t')}
-        </pre>
-      </Wrapper>
-    );
-  }
-}
+export const Repo = ({repo=null}) => (
+  repo &&
+    <Wrapper>
+      <Title>{repo.name}</Title>
+      <pre>
+        {JSON.stringify(repo, null, '\t')}
+      </pre>
+    </Wrapper>
+);
 
 const Wrapper = styled.div`
   color: red;
--- src/shared/containers/repo.js
+++ src/shared/containers/repo.js
@@ -2,5 +2,7 @@ import {connect} from 'react-redux';
 
 import {Repo} from '../components/repo';
 
-const mapStateToProps = ({repos}) => ({repos});
+const mapStateToProps = ({repos}, {match}) => ({
+  repo: repos.find((repo) => repo.name === match.params.repo)
+});
 export default connect(mapStateToProps)(Repo);
