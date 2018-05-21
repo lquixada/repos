13591ee41128010ca609c51db5c8ae94c0452561
Leonardo Quixadá <lquixada@gmail.com>
2018-05-21 03:33:05 -0300

    fixed contributors pagination.

--- src/shared/helpers/contributors.js
+++ src/shared/helpers/contributors.js
@@ -2,14 +2,22 @@ import fetch from './fetch';
 import {github} from '../config';
 
 export const extractNext = (header) => {
-  header = header || '';
-  const match = header.match(/<(.*?)>; rel="next"/);
+  const str = header || '';
+  const result = str
+    .split(',')
+    .find((fragment) => /rel="next"/.test(fragment));
+
+  if (!result) {
+    return '';
+  }
+
+  const match = result.match(/<(.*)>/);
   return match? match[1] : '';
 };
 
 export const fetchContributors = async (repoName) => {
   const endpoint = github.endpoints.contributors;
-  const url = `${endpoint.replace(':repo', repoName)}?page=1&per_page=40`;
+  const url = `${endpoint.replace(':repo', repoName)}?per_page=40`;
   const res = await fetch(url);
   const next = extractNext(res.headers.get('Link'));
   const result = await res.json();
