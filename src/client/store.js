63225ab94f3fe422c1605dc3dec9d806ea5f619c
Leonardo Quixadá <lquixada@gmail.com>
2018-05-21 01:27:39 -0300

    removed unneeded code.

--- src/client/store.js
+++ src/client/store.js
@@ -1,10 +1,6 @@
-import {routerMiddleware} from 'react-router-redux';
-
-import history from './history';
 import configureStore from '../shared/store';
 
 const state = window.__INITIAL_STATE__;
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const middlewares = [routerMiddleware(history)];
 
-export default configureStore(state, middlewares, devTools);
+export default configureStore(state, [], devTools);
