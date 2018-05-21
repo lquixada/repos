624eb6700578cdb8b5adb9f987f1319ad5282b84
Leonardo Quixadá <lquixada@gmail.com>
2018-05-21 02:55:06 -0300

    fixed regex for contributors count extraction.

--- src/shared/helpers/fetch.js
+++ src/shared/helpers/fetch.js
@@ -3,6 +3,6 @@ import fetch from 'cross-fetch';
 import {github} from '../config';
 
 export default (url) => {
-  const separator = (url.indexOf('?')>-1?'&':'?');
+  const separator = (url.indexOf('?')>-1? '&' : '?');
   return fetch(`${url}${separator}access_token=${github.accessToken}`);
 };
