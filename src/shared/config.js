a68682643710397b3a6de65c595616499651cc41
Leonardo Quixadá <lquixada@gmail.com>
2018-05-20 22:03:27 -0300

    placed token in config.

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,4 +1,7 @@
 export default {
+  // This token was created just for challenge purposes and has now admin privileges.
+  // It will expire very soon.
+  accessToken: '',
   endpoints: {
     contributors: 'https://api.github.com/repos/facebook/:repo/contributors',
     repo: 'https://api.github.com/repos/facebook/:repo',
--- src/shared/helpers/fetch.js
+++ src/shared/helpers/fetch.js
@@ -1,7 +1,8 @@
 import fetch from 'cross-fetch';
 
+import config from '../config';
+
 export default (url) => {
   const separator = (url.indexOf('?')>-1?'&':'?');
-  // This token has been added here temporarilly. It was created just for challenge purposes.
-  return fetch(`${url}${separator}access_token=`);
+  return fetch(`${url}${separator}access_token=${config.accessToken}`);
 };
