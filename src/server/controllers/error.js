4caf73a5edaa17992d845491b13d42f255c79a4e
Leonardo Quixadá <lquixada@gmail.com>
2018-05-23 16:15:42 -0300

    removed Provider from error template.

--- src/server/controllers/error.js
+++ src/server/controllers/error.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import {renderToString} from 'react-dom/server';
-import {Provider} from 'react-redux';
 import {StaticRouter} from 'react-router-dom';
 import {ServerStyleSheet, StyleSheetManager} from 'styled-components';
 
 import {NotFoundPage} from '../../shared/components/not-found-page';
 import template from '../templates/error';
-import configureStore from '../../shared/store';
 
 const errorMiddleware = (err, req, res, next) => {
   const sheet = new ServerStyleSheet();
   const html = renderToString(
     <StyleSheetManager sheet={sheet.instance}>
-      <Provider store={configureStore()}>
-        <StaticRouter location={req.url} context={{}}>
-          <NotFoundPage />
-        </StaticRouter>
-      </Provider>
+      <StaticRouter location={req.url} context={{}}>
+        <NotFoundPage />
+      </StaticRouter>
     </StyleSheetManager>
   );
 
