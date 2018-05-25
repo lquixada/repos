4c33b7569ddce3a4bf814d91ebc5e41fe567776f
Leonardo Quixadá <lquixada@gmail.com>
2018-05-25 20:04:42 -0300

    removed NotFound container.

--- src/shared/containers/not-found.js
+++
@@ -1,9 +0,0 @@
-import {NotFoundPage} from '../components/not-found-page';
-import {provideHooks} from '../helpers';
-import {loadHomePage} from '../actions';
-
-const hooks = {
-  fetch: ({dispatch}) => dispatch(loadHomePage()),
-};
-
-export default provideHooks(hooks)(NotFoundPage);
