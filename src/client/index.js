bb49326d7c2b1ac0af59037e6d9dd1327320e87c
Leonardo Quixadá <lquixada@gmail.com>
2018-05-25 00:18:50 -0300

    added no-ssr support.

--- src/client/index.js
+++ src/client/index.js
@@ -3,10 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
-import {renderRoutes} from 'react-router-config';
+import {renderRoutes, matchRoutes} from 'react-router-config';
 import {AppContainer} from 'react-hot-loader';
+import {END} from 'redux-saga';
 
 import {App} from '../shared/components/app';
+import {trigger} from '../shared/helpers';
 import routes from '../shared/routes'; // eslint-disable-line no-unused-vars
 import store from './store';
 
@@ -27,7 +29,22 @@ const renderApp = () => {
   );
 };
 
-renderApp();
+const matchs = matchRoutes(routes, window.location.pathname);
+
+if (matchs.length === 0) {
+  throw new Error(`React Router: Not found ${window.location.pathname}`);
+}
+
+const [{match, route}] = matchs;
+const {params} = match;
+
+if (!window.__INITIAL_STATE__) {
+  store.runnedSagas.toPromise().then(renderApp);
+  store.dispatch(trigger('fetch', route.component, params));
+  store.dispatch(END);
+} else {
+  renderApp();
+}
 
 if (module.hot) {
   module.hot.accept('../shared/routes', renderApp);
