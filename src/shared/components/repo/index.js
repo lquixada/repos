150593e4fb8c4f32008b7a57d939fc105762aaca
Leonardo Quixadá <lquixada@gmail.com>
2018-05-24 21:26:58 -0300

    simplified Repo container and component.

--- src/shared/components/repo/index.js
+++ src/shared/components/repo/index.js
@@ -5,15 +5,7 @@ import {Description} from './description';
 import {Header} from './header';
 import {Summary} from './summary';
 
-export const Repo = ({repo, hasLoaded}) => (
-  hasLoaded? <Loading /> : <Content repo={repo} />
-);
-
-const Loading = () => (
-  <div>Loading...</div>
-);
-
-const Content = ({repo}) => (
+export const Repo = ({repo}) => (
   <div>
     <Header title={repo.get('name')} url={repo.get('html_url')} />
     <Description text={repo.get('description')} />
