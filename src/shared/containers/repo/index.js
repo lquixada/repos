253d9757ace4a5b55751a7e205f752b8f5f00ac5
Leonardo Quixadá <lquixada@gmail.com>
2018-05-26 16:04:39 -0300

    fixed navigation from page to page.

--- src/shared/containers/repo/index.js
+++ src/shared/containers/repo/index.js
@@ -5,6 +5,10 @@ import {Repo} from '../../components/repo';
 import * as actions from '../../actions';
 
 export class RepoContainer extends React.Component {
+  componentDidMount() {
+    this.fetch();
+  }
+
   componentDidUpdate(prevProps) {
     if (this.hasChanged(prevProps)) {
       this.fetch();
