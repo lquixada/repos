71537af50683cbce5929cc300d584b9427730ce7
Leonardo Quixadá <lquixada@gmail.com>
2018-06-26 23:31:11 -0300

    removed quotes from jest.config.js.

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  'snapshotSerializers': ['enzyme-to-json/serializer'],
-  'testEnvironment': 'jsdom',
-  'testMatch': [
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testEnvironment: 'jsdom',
+  testMatch: [
     '<rootDir>/src/**/*.spec.js'
   ],
-  'setupTestFrameworkScriptFile': '<rootDir>/src/__tests__/setup.js',
-  'collectCoverage': true,
-  'coverageDirectory': '<rootDir>/.reports/coverage',
-  'coverageThreshold': {
-    'global': {
-      'branches': 80,
-      'functions': 90,
-      'lines': 90,
-      'statements': 90
+  setupTestFrameworkScriptFile: '<rootDir>/src/__tests__/setup.js',
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/.reports/coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 90,
+      lines: 90,
+      statements: 90
     }
   }
 };
