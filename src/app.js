d9489c6962add7f95f35831a7d11a9fab18293b3
Leonardo Quixadá <lquixada@gmail.com>
2018-06-29 16:53:31 -0300

    added localhost secure server.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import http from 'http';
+import https from 'https';
+
 import server from './server';
 
-const {PORT = 3000} = process.env;
+const PORT = 3000;
 
-server.listen(PORT, () => {
-  console.info(`Server running on: http://localhost:${PORT}/`);
-});
+if (process.env.SECURE) {
+  const sslPath = path.join(__dirname, '..', 'web', 'ssl');
+  const credentials = {
+    key: fs.readFileSync(path.join(sslPath, 'localhost.key')),
+    cert: fs.readFileSync(path.join(sslPath, 'localhost.cert')),
+    requestCert: false,
+    rejectUnauthorized: false,
+  };
+
+  https
+    .createServer(credentials, server)
+    .listen(PORT, () => {
+      console.info(`\n🔒 Secure server running on: https://localhost:${PORT}/`);
+    });
+} else {
+  http
+    .createServer(server)
+    .listen(PORT, () => {
+      console.info(`\n🔓 Insecure Server running on: http://localhost:${PORT}/`);
+    });
+}
