c0a9b8fb468bfc558ee3c09f6e8f01089bd2696f
Leonardo Quixadá <lquixada@gmail.com>
2018-07-09 23:50:15 -0300

    added cors to api.

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
 import loggerMiddleware from './middlewares/logger'
 import apiController from './controllers/api'
 
 const server = express()
 
+server.use(cors())
 server.use(loggerMiddleware)
 
 /* Controllers */
