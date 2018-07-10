a783b841b5f95d3ebdde219257fe90f1f1f06905
Leonardo Quixadá <lquixada@gmail.com>
2018-07-10 09:53:56 -0300

    disabled logging in file.

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,31 +1,17 @@
-import path from 'path'
 import winston from 'winston'
 
 const {combine, colorize, simple} = winston.format
 const isProd = process.env.NODE_ENV === 'production'
 const format = isProd ? simple() : combine(colorize(), simple())
 
-const transports = [
-  new winston.transports.Console({
-    level: 'debug',
-    silent: false,
-    format
-  })
-]
-
-if (!isProd) {
-  transports.push(
-    new winston.transports.File({
-      level: 'info',
-      filename: path.join(__dirname, '..', '..', 'logs', 'all.log'),
-      maxsize: 5242880, // 5MB
-      maxFiles: 5
-    })
-  )
-}
-
 const logger = winston.createLogger({
-  transports,
+  transports: [
+    new winston.transports.Console({
+      level: 'debug',
+      silent: false,
+      format
+    })
+  ],
   exitOnError: false
 })
 
