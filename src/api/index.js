5b68049cbe7d0ad485dd43d1754ca467557023e5
Leonardo Quixadá <lquixada@gmail.com>
2018-07-12 18:09:38 -0300

    removed /api endpoint.

--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import apollo from './apollo'
 
-import loggerMiddleware from './logger'
-import apiRouter from './router'
+import logger from './logger'
 
 const app = express()
 
 app.use(cors())
-app.use(loggerMiddleware)
-app.use('/api', apiRouter)
+app.use(logger)
 
 apollo.applyMiddleware({ app })
 
