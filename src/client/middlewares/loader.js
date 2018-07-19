2818958a117bd38331b36ac5dadbe318d4dbdfae
Leonardo Quixadá <lquixada@gmail.com>
2018-07-19 17:29:57 -0300

    disabled spinner.

--- src/client/middlewares/loader.js
+++ src/client/middlewares/loader.js
@@ -1,6 +1,8 @@
 import NProgress from 'nprogress'
 import {PAGE_REQUESTED, PAGE_SUCCEEDED, PAGE_FAILED} from '../../shared/actions'
 
+NProgress.configure({ showSpinner: false })
+
 export default store => next => action => {
   const { type } = action
 
