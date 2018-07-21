4e2a9f5b70e2b6ef110d9b40eac2bb132af8e880
Leonardo Quixadá <lquixada@gmail.com>
2018-07-20 23:35:54 -0300

    increased branch threshold coverage to 85%.

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
   coverageDirectory: '<rootDir>/.reports/coverage',
   coverageThreshold: {
     global: {
-      branches: 75,
+      branches: 85,
       functions: 85,
       lines: 85,
       statements: 85
