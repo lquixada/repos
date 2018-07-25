648a53573628c4a2f2ed91269e4a66cdb25d5d32
Leonardo Quixadá <lquixada@gmail.com>
2018-07-25 18:31:36 -0300

    renamed local.js to dev.js.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
   apps: [{
     name: 'web',
     interpreter: 'babel-node',
-    script: './src/server/local.js',
+    script: './src/server/dev.js',
     watch: ['./src/server']
   }, {
     name: 'api',
     interpreter: 'babel-node',
     interpreter_args: '--require node_modules/dotenv/config',
-    script: './src/api/local.js',
+    script: './src/api/dev.js',
     watch: ['./src/api']
   }]
 }
