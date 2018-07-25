9187622a9119504d606f0baba2db8573be2943fb
Leonardo Quixadá <lquixada@gmail.com>
2018-07-24 21:24:29 -0300

    removed cors middleware.

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,10 @@
 import express from 'express'
-import cors from 'cors'
 
 import logger from './logger'
 import apollo from './apollo'
 
 const app = express()
 
-app.use(cors())
 app.use(logger)
 
 apollo.applyMiddleware({ app })
