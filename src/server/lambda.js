ffe802bdc34b609d6e4a3f6f60212a5a0df727d7
Leonardo Quixadá <lquixada@gmail.com>
2018-07-25 12:19:24 -0300

    dropped aws-serverless-express in favor of serverless-http.

--- src/server/lambda.js
+++ src/server/lambda.js
@@ -1,6 +1,4 @@
-import awsServerlessExpress from 'aws-serverless-express'
+import http from 'serverless-http'
 import app from './index'
 
-const server = awsServerlessExpress.createServer(app)
-
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
+exports.handler = http(app)
